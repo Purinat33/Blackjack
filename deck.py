706f86a8738b37ea55eebb6e25a6b0918553417f
Purinat33 <Purinat33@gmail.com>
2023-10-14 22:53:17 +0700

    Got player class setup

--- deck.py
+++ deck.py
@@ -12,6 +12,8 @@ class Deck:
     
 
     def print_deck(self):
+        # We are not really using this function in the game really
+        # Because we only wants to show only cards on each player's hand
         count = 0
         for card in self.cards:
             print(f'{card[0]} of {card[1]}', end='\t')
@@ -22,12 +24,36 @@ class Deck:
                            
     def shuffle(self):
         random.shuffle(self.cards)
+        
+    
+    def deal_card(self, isFaceUp: bool) -> (str, str):
+        if isFaceUp:
+            drawn_card = self.cards.pop()
+            print("Dealt: ", drawn_card)
+            return drawn_card
+        else:
+            print("Dealth a card")
+            return self.cards.pop()
     
     
 class Player:
+    
     def __init__(self):
-        self.score = 0 # This will be useful when counting later?
+        self.score = 0 # This will be useful when counting later
         self.hands = []
+        
+    def hand_score(self): # List of cards on hand
+        score = 0
+        card_score = {'Ace': 11, '2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'Jack': 10, 'Queen': 10, 'King': 10}
+        for card in self.hands:
+            rank = card[0]
+            score += card_score[rank]
+        
+        return score    
+    
+    
+    def print_hand(self):
+        pass
         
         
     
--- main.py
+++ main.py
@@ -6,3 +6,5 @@ if __name__ == '__main__':
     poker.shuffle()
     print('-----------')
     poker.print_deck()
+    print(poker.deal_card(True))
+    print(poker.deal_card(True))
